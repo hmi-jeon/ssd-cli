1ac4ec68440daaa8d1b1f4a6560a944a0d694085
kimyoungsik <addi97@naver.com>
2024-04-16 13:32:22 +0900

    [Test Shell] write 기능 추가

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -1,6 +1,18 @@
-#include "pch.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "../ssd-cli-test/TestShell.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class MockTestShell : public TestShell {
+public:
+	MOCK_METHOD(void, write, (int, string), ());
+};
+
+TEST(TestCaseName, TestWrite) {
+	MockTestShell testShell;
+	EXPECT_CALL(testShell, write(10, "0xABDCDDDD"))
+		.Times(1);
+	
+	testShell.write(10, "0xABDCDDDD");
 }
+
+
---
+++ ssd-cli-test/TestShell.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+class TestShell {
+public:
+	void write(int LBA, string data ) {
+		
+	}
+
+private:
+};
