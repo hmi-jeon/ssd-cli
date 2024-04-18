ad6497b30488414cebdc3d6efed0d59dcfb51440
kimyoungsik <addi97@naver.com>
2024-04-18 15:54:18 +0900

    [refactor] arguments가 1개가 아닐 경우 bugfix

--- ssd-cli-test/main.cpp
+++ ssd-cli-test/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main(int argc, char **argv, char **envp) {
 
 	TestShell ssdTestShell;
-	string firstArg = argv[1];
 
-	if (argc == 2 && firstArg == RUN_LIST) {
-		ssdTestShell.Runner();
+	if (argc == 2) {
+		string firstArg = argv[1];
+		if(firstArg == RUN_LIST) ssdTestShell.Runner();
 	}
 
 	if (argc == 1) {
