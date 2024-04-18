f6f0e7e0d772c53dae793dd932ae164b41d0d640
ykwryu <ykw.ryu@samsung.com>
2024-04-18 14:00:02 +0900

    [BugFix] fix localtime "'localtime': This function or variable may be unsafe." error

--- ssd-cli-test/Logger.cpp
+++ ssd-cli-test/Logger.cpp
@@ -59,7 +59,9 @@ private:
 
 	string makeDateString(DateType dateType) {
 		std::time_t now = std::time(nullptr);
-		std::tm* current_time = std::localtime(&now);
+		std::tm* current_time;
+		localtime_s(current_time, &now);
+
 		stringstream ss{};
 		switch (dateType)
 		{
