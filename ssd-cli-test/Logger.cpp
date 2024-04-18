a36f21a2f2085cbd0e409c7c5b344e42800dc2a6
ykwryu <ykw.ryu@samsung.com>
2024-04-18 12:02:40 +0900

    latest file size check 기능 추가

--- ssd-cli-test/Logger.cpp
+++ ssd-cli-test/Logger.cpp
@@ -17,8 +17,6 @@ enum LoggerMode {
 
 class Logger {
 public:
-	static constexpr char LATEST_LOG_FILE_NAME[11] = "latest.log";
-
 	static Logger& getInstance() {
 		static Logger instance;
 		return instance;
@@ -26,30 +24,6 @@ public:
 
 	void setLoggerMode(LoggerMode mode) {
 		this->mode = mode;
-	}
-	bool openLogFile(fstream& log_fs, const std::string& filename) {
-		log_fs.open(filename, std::ios::out);
-		if (!log_fs.is_open()) {
-			return false;
-		}
-	}
-	void saveLogger(string logBuffer) {
-		openLogFile(log_fs, LATEST_LOG_FILE_NAME);
-		log_fs << logBuffer << endl;
-		log_fs.close();
-	}
-
-	string makeDateString() {
-		std::time_t now = std::time(nullptr);
-		std::tm* current_time = std::localtime(&now);
-		stringstream ss{};
-		ss << "[" << std::put_time(current_time, "%y.%m.%d %H:%M:%S") << "]";
-		return ss.str();
-	}
-
-	bool makeLog(string& logBuffer, string functionName, string logMsg) {
-		logBuffer = makeDateString() + functionName + ":" + logMsg;
-		return true;
 	}
 
 	void log(string functionName, string logMsg) {
@@ -72,4 +46,60 @@ private:
 	Logger& operator=(const Logger&) = delete;
 	fstream log_fs;
 
+	static constexpr char LATEST_LOG_FILE_NAME[11] = "latest.log";
+	static constexpr int LIMIT_LOG_SIZE = 200;// 1024 * 10; // 10KB
+
+
+	
+	streampos checkFileSize(const std::string& filename) {
+		std::ifstream file(filename, std::ios::binary);
+
+		if (!file.is_open()) {
+			return -1; // 에러 시 -1 반환
+		}
+
+		file.seekg(0, std::ios::end);
+		std::streampos size = file.tellg();
+
+		file.close();
+
+		return size;
+	}
+
+	bool openLogFile(fstream& log_fs, const std::string& filename) {
+		log_fs.open(filename, std::ios::out | std::ios::app);
+		if (!log_fs.is_open()) {
+			return false;
+		}
+	}
+
+	void saveUntilLog()	{		
+		cout << "saveUntilLog : file size over" << endl;
+	}
+
+	void saveLogger(string logBuffer) {
+
+		streampos latestLogFilesize = checkFileSize(LATEST_LOG_FILE_NAME);
+		if(latestLogFilesize == -1) return;
+		if (static_cast<int>(latestLogFilesize) + logBuffer.size() > LIMIT_LOG_SIZE){
+			saveUntilLog();
+		}
+
+		openLogFile(log_fs, LATEST_LOG_FILE_NAME);
+		log_fs << logBuffer << endl;
+		log_fs.close();
+	}
+
+	string makeDateString() {
+		std::time_t now = std::time(nullptr);
+		std::tm* current_time = std::localtime(&now);
+		stringstream ss{};
+		ss << "[" << std::put_time(current_time, "%y.%m.%d %H:%M:%S") << "]";
+		return ss.str();
+	}
+
+	bool makeLog(string& logBuffer, string functionName, string logMsg) {
+		logBuffer = makeDateString() + functionName + ":" + logMsg;
+		return true;
+	}
 };
