53f751dd97483ca0e8f29332e22e29baef884c03
ykwryu <ykw.ryu@samsung.com>
2024-04-23 10:36:58 +0900

    [refacotr] REPEAT_READ_TEST

--- REPEAT_READ_TEST/REPEAT_READ_TEST.cpp
+++ REPEAT_READ_TEST/REPEAT_READ_TEST.cpp
@@ -6,30 +6,16 @@
 #include "../ssd-cli-test/Logger.hpp"
 
 #define MAX_SIZE 5
-#define LOOP_MAX_SIZE 50
 
 using namespace std;
 
 const string APP_NAME = "ssd.exe";
 const string RESULT_FILE = "result.txt";
 
-//void write(const int lba, const string data) {
-//	string command = APP_NAME + " " + "W" + " " + to_string(lba) + " " + data;
-//	system(command.c_str());
-//}
-
 void read(const int lba) {
 	string command = APP_NAME + " " + "R" + " " + to_string(lba);
 	system(command.c_str());
 }
-
-//string getResultFile() {
-//	ifstream resultFile;
-//	string data = "";
-//	resultFile.open(RESULT_FILE);
-//	if (resultFile.is_open()) resultFile >> data;
-//	return data;
-//}
 
 int main(int argc, char* argv[]) {
 	Logger& logger = Logger::getInstance();
